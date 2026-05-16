29da5c334861f7debbe3f570c24c2d4602889ae7
nitz8690 <nishant_shah0806@gmail.com>
2026-05-17 00:05:23 +0200

    updated setting.py

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -129,7 +129,11 @@ CONTACT_EMAIL = os.getenv('CONTACT_EMAIL', 'nishant.shah0806@gmail.com')
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio', 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
